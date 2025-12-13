7d64d48267a46a32d2cded9304d1c607c7d79d2c
matheus-vsm <scompinha@hotmail.com>
2025-12-12 23:36:17 -0300

    aula 2.7

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -1,13 +1,12 @@
 package br.com.alura.forum.controller
 
-import br.com.alura.forum.model.Curso
 import br.com.alura.forum.model.Topico
-import br.com.alura.forum.model.Usuario
 import br.com.alura.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.util.Arrays
 
 @RestController
 @RequestMapping("/topicos")
@@ -18,6 +17,11 @@ class TopicoController(
     @GetMapping
     fun listar(): List<Topico> {
         return service.listar()
+    }
+
+    @GetMapping("/{id}")
+    fun buscarPorId(@PathVariable id: Long): Topico {
+        return service.buscarPorId(id)
     }
 
 }
--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Service
 import java.util.Arrays
 
 @Service
-class TopicoService {
+class TopicoService(
+    private var topicos: List<Topico>
+) {
 
-    fun listar(): List<Topico> {
+    init {
         val topico = Topico(
             id = 1,
             titulo = "Duvidas Spring",
@@ -25,8 +27,46 @@ class TopicoService {
                 email = "Matheus@Spring.com",
             )
         )
+        val topico2 = Topico(
+            id = 2,
+            titulo = "Duvidas Spring2",
+            mensagem = "Mensagem2",
+            curso = Curso(
+                id = 2,
+                nome = "Spring2",
+                categoria = "Programacao2"
+            ),
+            autor = Usuario(
+                id = 2,
+                nome = "Matheus2",
+                email = "Matheus@Spring.com2",
+            )
+        )
+        val topico3 = Topico(
+            id = 3,
+            titulo = "Duvidas Spring3",
+            mensagem = "Mensagem3",
+            curso = Curso(
+                id = 3,
+                nome = "Spring3",
+                categoria = "Programacao3"
+            ),
+            autor = Usuario(
+                id = 3,
+                nome = "Matheus3",
+                email = "Matheus@Spring.com3",
+            )
+        )
 
-        return Arrays.asList(topico, topico, topico)
+        topicos = Arrays.asList(topico, topico2, topico3)
+    }
+
+    fun listar(): List<Topico> {
+        return topicos
+    }
+
+    fun buscarPorId(id: Long): Topico {
+        return topicos.filter { it.id == id }.first()
     }
 
 }
