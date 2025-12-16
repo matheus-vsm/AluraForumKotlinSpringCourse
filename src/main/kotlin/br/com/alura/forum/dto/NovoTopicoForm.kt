9f08e7edd828e130e7136f9757c1ce38f7d867e2
matheus-vsm <scompinha@hotmail.com>
2025-12-15 23:15:50 -0300

    aula 5.6

--- src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
+++ src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
@@ -5,10 +5,10 @@ import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
 data class NovoTopicoForm(
-    @field:NotEmpty
-    @field:Size(min = 1, max = 100)
+    @field:NotEmpty(message = "Titulo não pode ser vazio!")
+    @field:Size(min = 1, max = 100, message = "Titulo não pode ser vazio e deve ter entre 1 e 100 caracteres!")
     val titulo: String,
-    @field:NotEmpty
+    @field:NotEmpty(message = "Mensagem não pode ser vazia e deve ter entre 1 e 200 caracteres!")
     @field:Size(min = 1, max = 200)
     val mensagem: String,
     @field:NotNull
--- src/main/kotlin/br/com/alura/forum/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/br/com/alura/forum/exception/GlobalExceptionHandler.kt
@@ -3,6 +3,7 @@ package br.com.alura.forum.exception
 import br.com.alura.forum.dto.ErrorView
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -21,6 +22,26 @@ class GlobalExceptionHandler {
             status = HttpStatus.NOT_FOUND.value(),
             error = HttpStatus.NOT_FOUND.name,
             message = exception.message ?: "Recurso não encontrado",
+            path = request.servletPath
+        )
+    }
+
+    // tratamento 400 BAD REQUEST
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationFound(
+        exception: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ErrorView {
+        val errorMessage = HashMap<String, String?>()
+        // recupera os erros que aconteceram
+        exception.bindingResult.fieldErrors.forEach { e ->
+            errorMessage.put(e.field, e.defaultMessage)
+        }
+        return ErrorView(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = errorMessage.toString(),
             path = request.servletPath
         )
     }
