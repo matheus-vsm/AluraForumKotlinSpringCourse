852db4bf1811029791e39df16ce76c1c2b81b118
matheus-vsm <scompinha@hotmail.com>
2025-12-17 23:55:24 -0300

    aula 5.6

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -8,6 +8,7 @@ import br.com.alura.forum.exception.NotFoundException
 import br.com.alura.forum.mapper.TopicoFormMapper
 import br.com.alura.forum.mapper.TopicoViewMapper
 import br.com.alura.forum.repository.TopicoRepository
+import jakarta.persistence.EntityManager
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
@@ -17,13 +18,15 @@ class TopicoService(
     private val repository: TopicoRepository,
     private val topicoViewMapper: TopicoViewMapper,
     private val topicoFormMapper: TopicoFormMapper,
-    private val notFoundMessage: String = "Topico não encontrado"
+    private val notFoundMessage: String = "Topico não encontrado",
+    private val em: EntityManager
 ) {
 
     fun listar(
         nomeCurso: String?,
         paginacao: Pageable
     ): Page<TopicoView> {
+        print(em)
         val topicos = if (nomeCurso == null) repository.findAll(paginacao)
         else repository.findByCursoNome(nomeCurso, paginacao)
 
