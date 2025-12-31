16e858fff1dce3e644dc0728cde47f81ad8e79cd
matheus-vsm <scompinha@hotmail.com>
2025-12-30 23:43:32 -0300

    aula 3.5

--- src/test/kotlin/br/com/alura/forum/integration/TopicoRepositoryTest.kt
+++ src/test/kotlin/br/com/alura/forum/integration/TopicoRepositoryTest.kt
@@ -1,38 +1,62 @@
-package br.com.alura.forum.integration
-
-import br.com.alura.forum.repository.TopicoRepository
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase
-import org.springframework.security.core.userdetails.User.withUsername
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
-import org.testcontainers.containers.MySQLContainer
-import org.testcontainers.junit.jupiter.Container
-import org.testcontainers.junit.jupiter.Testcontainers
-
-@DataJpaTest
-@Testcontainers
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class TopicoRepositoryTest {
-
-    @Autowired
-    private lateinit var repository: TopicoRepository
-
-    companion object {
-        @Container
-        private val mysqlContainer: MySQLContainer<Nothing>("mysql:8.0.28").apply {
-            withDatabaseName("testdb")
-            withUsername("testuser")
-            withPassword("testpass")
-        }
-
-        @JvmStatic
-        @DynamicPropertySource
-        fun properties(registry: DynamicPropertyRegistry) {
-            registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl)
-            registry.add("spring.datasource.username", mysqlContainer::getUsername)
-            registry.add("spring.datasource.password", mysqlContainer::getPassword)
-        }
-    }
-
-}
+//package br.com.alura.forum.integration
+//
+//import br.com.alura.forum.dto.TopicoPorCategoriaDto
+//import br.com.alura.forum.model.TopicoTest
+//import br.com.alura.forum.repository.TopicoRepository
+//import org.assertj.core.api.Assertions.assertThat
+//import org.junit.jupiter.api.Test
+//import org.springframework.beans.factory.annotation.Autowired
+//import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+//import org.springframework.data.domain.PageRequest
+//import org.springframework.test.context.DynamicPropertyRegistry
+//import org.springframework.test.context.DynamicPropertySource
+//import org.testcontainers.containers.MySQLContainer
+//import org.testcontainers.junit.jupiter.Container
+//import org.testcontainers.junit.jupiter.Testcontainers
+//
+//@DataJpaTest
+//@Testcontainers
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//class TopicoRepositoryTest{
+//
+//    @Autowired
+//    private lateinit var topicoRepository: TopicoRepository
+//
+//    private val paginacao = PageRequest.of(0,5)
+//    private val topico = TopicoTest.build()
+//
+//    companion object {
+//        @Container
+//        private val mysqlContainer = MySQLContainer<Nothing>("mysql:latest").apply {
+//            withDatabaseName("testdb")
+//            withUsername("joao")
+//            withPassword("12345")
+//        }
+//
+//        @JvmStatic
+//        @DynamicPropertySource
+//        fun properties(registry: DynamicPropertyRegistry) {
+//            registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
+//            registry.add("spring.datasource.password", mysqlContainer::getPassword);
+//            registry.add("spring.datasource.username", mysqlContainer::getUsername);
+//        }
+//    }
+//
+//    @Test
+//    fun deveGerarUmRelatorio() {
+//        topicoRepository.save(topico)
+//        val relatorio = topicoRepository.relatorio()
+//
+//        assertThat(relatorio).isNotNull
+//        assertThat(relatorio.first()).isExactlyInstanceOf(TopicoPorCategoriaDto::class.java)
+//    }
+//
+//    @Test
+//    fun deveListarTopicoPeloNomeDoCurso() {
+//        topicoRepository.save(topico)
+//        val topico = topicoRepository.findByCursoNome(topico.curso.nome, PageRequest.of(0,5))
+//
+//        assertThat(topico).isNotNull
+//    }
+//}
