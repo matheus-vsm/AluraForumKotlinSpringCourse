84d3d4aff0f3443f6e42760a32397dee00a20c61
matheus-vsm <scompinha@hotmail.com>
2026-01-02 22:22:32 -0300

    aula 4.3

--- src/test/kotlin/br/com/alura/forum/controller/TopicoControllerTest.kt
+++ src/test/kotlin/br/com/alura/forum/controller/TopicoControllerTest.kt
@@ -1,10 +1,12 @@
 package br.com.alura.forum.controller
 
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
@@ -17,6 +19,10 @@ class TopicoControllerTest {
     @Autowired
     private lateinit var webApplicationContext: WebApplicationContext
 
+    companion object {
+        private const val RECURSO = "/topicos"
+    }
+
     @BeforeEach // Configurações iniciais antes de cada teste
     fun setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
@@ -24,6 +30,15 @@ class TopicoControllerTest {
                 SecurityMockMvcConfigurers.springSecurity()
             )
             .build()
+    }
+
+    @Test
+    fun deveRetornarCodigo400QuandoChamarTopicosSemToken() {
+        mockMvc.get(RECURSO).andExpect {
+            status {
+                is4xxClientError()
+            }
+        }
     }
 
 }
